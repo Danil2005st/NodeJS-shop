a9e8c36a9e43018ae599875da29041b1f65d4a3e
danilaprokopenko <danila.prokopenko@dataart.com>
2025-01-23 17:05:10 +0200

    Updated with mongoDB

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req,res,next)=>{
   User.findById('6790ece68f6d0197c869f1c1').then(user=>{
-    req.user = user;
+    req.user = new User(user.name, user.email, user.cart, user._id);
     next();
   }).catch(err=>{console.log(err)});
 });
